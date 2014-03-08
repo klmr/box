949519876ccb2ca4d58054c2abd11f9c5b2e446c
Konrad Rudolph <konrad.rudolph@gmail.com>
2014-03-08 17:34:03 +0000

    Let `import` retrieve the module’s code location

--- R/import.R
+++ R/import.R
@@ -1,6 +1,11 @@
 #' @export
-import = function (module, attach = TRUE)
-    NULL
+import = function (module, attach = TRUE) {
+    stopifnot(is(substitute(module), 'name'))
+    module_path = try(find_module(substitute(module)), silent = TRUE)
+
+    if (is(module_path, 'try-error'))
+        stop(attr(module_path, 'condition')$message)
+}
 
 #' @export
 unload = function (module)
