29565b207eabfc9a37d2716881fa9f8f1def3e6a
Konrad Rudolph <konrad.rudolph@gmail.com>
2014-03-08 19:51:28 +0000

    Save which modules have been loaded

--- R/import.R
+++ R/import.R
@@ -1,11 +1,23 @@
 #' @export
 import = function (module, attach = TRUE) {
-    stopifnot(is(substitute(module), 'name'))
-    module_path = try(find_module(substitute(module)), silent = TRUE)
+    module = substitute(module)
+    stopifnot(is(module, 'name'))
+    module_path = try(find_module(module), silent = TRUE)
 
     if (is(module_path, 'try-error'))
         stop(attr(module_path, 'condition')$message)
+
+    if (is_module_loaded(module))
+        return(invisible())
 }
+
+.loaded_modules = c()
+
+is_module_loaded = function (module)
+    as.character(module) %in% .loaded_modules
+
+mark_module_loaded = function (module)
+    '.loaded_modules' <<- c(.loaded_modules, as.character(module))
 
 #' @export
 unload = function (module)
