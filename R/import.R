70393eb90d98bdd9e8d13d5353de2b886a6b8be7
Konrad Rudolph <konrad.rudolph@gmail.com>
2014-03-11 22:37:12 +0000

    Move module caching to its own file

--- R/import.R
+++ R/import.R
@@ -31,28 +31,7 @@ import = function (module, attach = TRUE) {
     invisible(module_env)
 }
 
-#' Environment of loaded modules
 #'
-#' Each module is stored as an environment inside \code{.loaded_modules} with
-#' the module’s code location path as its identifier. The path rather than the
-#' module name is used because module names are not unique: two modules called
-#' \code{a} can exist nested inside modules \code{b} and \code{c}, respectively.
-#' Yet these may be loaded at the same time and need to be distinguished.
-.loaded_modules = new.env()
-
-is_module_loaded = function (module_path)
-    exists(module_path, envir = .loaded_modules)
-
-mark_module_loaded = function (module_env) {
-    assign(module_path(module_env), module_env, envir = .loaded_modules)
-}
-
-get_loaded_module = function (module_path)
-    get(module_path, envir = .loaded_modules)
-
-module_path = function (module_env)
-    parent.env(module_env)$module_path
-
 #' @export
 unload = function (module)
     NULL
