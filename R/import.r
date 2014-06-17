4a8b35c3db0cc5ea455a31adcc43882e71e82fe3
Konrad Rudolph <konrad.rudolph@gmail.com>
2014-06-17 16:59:44 +0100

    Don’t change directory to imported source file
    Remove `chdir` argument to `source` in `import`, which is logically not
    necessary any more, and was never desired.

--- R/import.r
+++ R/import.r
@@ -116,8 +116,7 @@ do_import = function (module_name, module_path) {
                           name = paste('namespace', module_name, sep = ':'),
                           path = module_path,
                           class = c('namespace', 'environment'))
-    local(source(attr(environment(), 'path'), chdir = TRUE, local = TRUE),
-          envir = namespace)
+    local(source(attr(environment(), 'path'), local = TRUE), envir = namespace)
     cache_module(namespace)
     namespace
 }
