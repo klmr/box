d62e7e3b479781182c1a756697bc734e4a690b04
Konrad Rudolph <konrad.rudolph@gmail.com>
2014-06-19 11:53:34 +0100

    Adjust test import paths

--- inst/tests/test-relative-import.r
+++ inst/tests/test-relative-import.r
@@ -1,11 +1,11 @@
 context('Relative imports test')
 
 test_that('Imports are absolute by default', {
-    ra = import('relative_a')
+    ra = import('./modules/nested/relative_a')
     expect_that(ra$a_which(), equals('/a'))
 })
 
 test_that('Relative import are always local', {
-    ra = import('relative_a')
+    ra = import('./modules/nested/relative_a')
     expect_that(ra$local_a_which(), equals('nested/a'))
 })
