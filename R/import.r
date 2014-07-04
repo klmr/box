f25cf1d421eb87867a59fa4661cf49ffcc9645ce
Konrad Rudolph <konrad.rudolph@gmail.com>
2014-07-04 16:00:13 +0100

    Fix bug with modules exporting only one symbol

--- R/import.r
+++ R/import.r
@@ -151,7 +151,8 @@ exhibit_namespace = function (namespace, name, parent, export_list) {
     # Skip one parent environment because this module is hooked into the chain
     # between the calling environment and its ancestor, thus sitting in its
     # local object search path.
-    structure(list2env(sapply(export_list, get, envir = namespace),
+    structure(list2env(sapply(export_list, get, envir = namespace,
+                              simplify = FALSE),
                        parent = parent.env(parent)),
               name = paste('module', name, sep = ':'),
               path = module_path(namespace),
