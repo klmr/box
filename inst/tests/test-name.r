81be195bdbdb406a0e1c3aada9f0d2225df49937
Konrad Rudolph <konrad.rudolph@gmail.com>
2015-06-26 18:48:10 +0100

    Replace faulty function `expect`
    A recent update of the `testthat` package broke the `expect` function I
    had defined. Since we don’t actually require this function, I’ve
    replaced it with its equivalent `testthat` functions.

--- inst/tests/test-name.r
+++ inst/tests/test-name.r
@@ -1,16 +1,16 @@
 context('Module names')
 
 test_that('the global namespace has no module name', {
-    expect(is.null(module_name()))
+    expect_null(module_name())
 })
 
 test_that('modules have a name', {
     a = import('a')
-    expect(module_name(a) == 'a')
-    expect(a$get_modname() == 'a')
+    expect_equal(module_name(a), 'a')
+    expect_equal(a$get_modname(), 'a')
 })
 
 test_that('module names can be read inside functions', {
     a = import('a')
-    expect(a$get_modname2() == 'a')
+    expect_equal(a$get_modname2(), 'a')
 })
