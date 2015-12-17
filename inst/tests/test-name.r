74b1323ebc491fa8ef3b9d556fa44dc5450afef6
Konrad Rudolph <konrad.rudolph@gmail.com>
2015-12-17 13:24:00 +0100

    Add unit tests for `module_name`
    Same issue as for `module_file`, exacerbated by the fact that the name
    needs to be maintained both as a `module_attr` and as a conventional
    `attr` to satisfy `namespaceName`.

--- inst/tests/test-name.r
+++ inst/tests/test-name.r
@@ -14,3 +14,16 @@ test_that('module names can be read inside functions', {
     a = import('a')
     expect_equal(a$get_modname2(), 'a')
 })
+
+test_that('module_name works after attaching modules', {
+    # Test that #66 is fixed and that there are no regressions.
+
+    x = import('mod_name')
+
+    expect_that(x$this_module_name, equals('mod_name'))
+    expect_that(x$function_module_name(), equals('mod_name'))
+    expect_that(x$this_module_name2, equals('mod_name'))
+    expect_that(x$after_module_attach(), equals('mod_name'))
+    expect_that(x$after_package_attach(), equals('mod_name'))
+    expect_that(x$nested_module_name(), equals('mod_name'))
+})
