9f8dde1131d8a6f37df912afa45763c41cce4536
Konrad Rudolph <konrad.rudolph@gmail.com>
2017-01-11 17:11:29 +0000

    Add documentation for path functions

--- R/find_module.r
+++ R/find_module.r
@@ -129,6 +129,13 @@ import_search_path = function () {
     c(getOption('import.path', environment), module_base_path(parent.frame()))
 }
 
+#' Split a path into its components and merge them back together
+#'
+#' \code{split_path(path)} is a platform independent and file system logic
+#' aware alternative to \code{strsplit(path, '/')[[1]]}.
+#' @param path the path to split
+#' @return \code{split_path} returns a character vector of path components that
+#' logically represent \code{path}.
 split_path = function (path) {
     if (identical(path, dirname(path)))
         path
@@ -136,6 +143,15 @@ split_path = function (path) {
         c(Recall(dirname(path)), basename(path))
 }
 
+#' \code{merge_path(split_path(path))} is equivalent to \code{path}.
+#' @return \code{merge_path} returns a single character string that is
+#' logically equivalent to the \code{path} passed to \code{split_path}.
+#' logically represent \code{path}.
+#' @note \code{merge_path} is the inverse function to \code{split_path}.
+#' However, this does not mean that its result will be identical to the
+#' original path. Instead, it is only guaranteed that it will refer to the same
+#' logical path given the same working directory.
+#' @rdname split_path
 merge_path = function (components) {
     do.call(file.path, as.list(components))
 }
