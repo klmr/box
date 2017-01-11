bd1021b60dda5b70b679f089e5457d043834ad46
Konrad Rudolph <konrad.rudolph@gmail.com>
2017-01-11 18:47:54 +0000

    Vectorize `realpath` function
    Like `normalizePath`, `realpath` now takes a vector of paths and
    dispatches them to a single shell call.

--- inst/tests/helper-paths.r
+++ inst/tests/helper-paths.r
@@ -1,8 +1,19 @@
+#' Compute normalized logical paths
+#'
+#' \code{realpath(path)} will return the normalized logical path for
+#' \code{path}, similar to \code{normalizePath} but working correctly for
+#' nonexistent paths on Unix systems.
+#' @param path a character vector of paths
+#' @note This function doesn’t work with paths containing newlines; this is a
+#' restriction of R because it truncates nul characters and thus cannot pass
+#' the \code{-z} flag to the \code{realpath} shell function.
 realpath = function (path) {
-    if (.Platform$OS.type == 'unix')
-        system(paste('realpath -m -s', shQuote(path.expand(path))), intern = TRUE)
-    else
+    if (.Platform$OS.type == 'unix') {
+        path_args = paste(shQuote(path.expand(path)), collapse = ' ')
+        system(paste('realpath -m -s', path_args), intern = TRUE)
+    } else {
         normalizePath(path, mustWork = FALSE)
+    }
 }
 
 expect_paths_equal = function (actual, expected) {
