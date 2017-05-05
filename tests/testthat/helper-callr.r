db725bfa2ea73b853999ec625696b7462e23df54
Konrad Rudolph <konrad.rudolph@gmail.com>
2017-05-05 17:02:27 +0100

    Extract call R test helpers into helper file

---
+++ tests/testthat/helper-callr.r
@@ -0,0 +1,41 @@
+rcmd = function (script_path) {
+    cmd = 'R CMD BATCH --slave --vanilla --no-restore --no-save --no-timing'
+    output_file = 'output.rout'
+    on.exit(unlink(output_file))
+    system(paste(cmd, script_path, output_file))
+    readLines(output_file)
+}
+
+rscript = function (script_path) {
+    cmd = 'Rscript --slave --vanilla --no-restore --no-save'
+    p = pipe(paste(cmd, script_path))
+    on.exit(close(p))
+    readLines(p)
+}
+
+interactive_r = function (script_path) {
+    cmd = 'R --vanilla --interactive'
+    output_file = 'output.rout'
+    on.exit(unlink(output_file))
+
+    local({
+        p = pipe(paste(cmd, '>', output_file), 'w')
+        on.exit(close(p))
+        writeLines(readLines(script_path), p)
+        writeLines('interactive()', p)
+    })
+
+    result = readLines(output_file)
+
+    check_line = function (which, expected)
+        if (! identical(result[which], expected))
+            stop('Unexpected value ', sQuote(result[which]), ', expected ',
+                 sQuote(expected), ' in `interactive_r`')
+
+    # Ensure that code was actually run interactively.
+    end = length(result)
+    check_line(end - 2, '> interactive()')
+    check_line(end - 1, '[1] TRUE')
+    check_line(end, '> ')
+    result[1 : (end - 3)]
+}
--- tests/testthat/test-path.r
+++ tests/testthat/test-path.r
@@ -14,48 +14,6 @@ test_that('module_file works for module', {
     expect_that(length(module_file(c('b', 'c.r'), module = a)), equals(2))
 })
 
-rcmd = function (script_path) {
-    cmd = 'R CMD BATCH --slave --vanilla --no-restore --no-save --no-timing'
-    output_file = 'output.rout'
-    on.exit(unlink(output_file))
-    system(paste(cmd, script_path, output_file))
-    readLines(output_file)
-}
-
-rscript = function (script_path) {
-    cmd = 'Rscript --slave --vanilla --no-restore --no-save'
-    p = pipe(paste(cmd, script_path))
-    on.exit(close(p))
-    readLines(p)
-}
-
-interactive_r = function (script_path) {
-    cmd = 'R --vanilla --interactive'
-    output_file = 'output.rout'
-    on.exit(unlink(output_file))
-
-    local({
-        p = pipe(paste(cmd, '>', output_file), 'w')
-        on.exit(close(p))
-        writeLines(readLines(script_path), p)
-        writeLines('interactive()', p)
-    })
-
-    result = readLines(output_file)
-
-    check_line = function (which, expected)
-        if (! identical(result[which], expected))
-            stop('Unexpected value ', sQuote(result[which]), ', expected ',
-                 sQuote(expected), ' in `interactive_r`')
-
-    # Ensure that code was actually run interactively.
-    end = length(result)
-    check_line(end - 2, '> interactive()')
-    check_line(end - 1, '[1] TRUE')
-    check_line(end, '> ')
-    result[1 : (end - 3)]
-}
-
 test_that('module_base_path works', {
     # On earlier versions of “devtools”, this test reproducibly segfaulted due
     # to the call to `load_all` from within a script. This seems to be fixed now
