b3efef66d0f3428b2739fb28a9fe4997f8327c6c
Konrad Rudolph <konrad.rudolph@gmail.com>
2017-05-13 16:01:41 +0100

    Replace (non macOS non-existent) `realpath`
    Implement an equivalent of GNU `realpath -m -s` in pure R.

--- tests/testthat/helper-paths.r
+++ tests/testthat/helper-paths.r
@@ -9,11 +9,46 @@
 #' the \code{-z} flag to the \code{realpath} shell function.
 realpath = function (path) {
     if (.Platform$OS.type == 'unix') {
-        path_args = paste(shQuote(path.expand(path)), collapse = ' ')
-        system(paste('realpath -m -s', path_args), intern = TRUE)
+        normalize_path(path)
     } else {
         normalizePath(path, mustWork = FALSE)
     }
+}
+
+#' Replacement for \code{normalizePath} that works with non-existent paths on
+#' Unix
+#'
+#' @param path vector of paths to normalize.
+#' @details
+#' \code{normalize_path} works like \code{\link[base]{normalizePath}}, but also
+#' works on non-existent paths. \code{\link[base]{normalizePath}} has an option
+#' \code{mustWork = FALSE} for this; however, that option only works on Windows.
+#' On Unix systems, it causes paths to be left untouched. However, and as a
+#' consequence, \code{normalize_path} does \emph{not} resolve symlinks.
+normalize_path = function (path) {
+    path = path.expand(path)
+
+    # Make absolute.
+    relative_paths = grep('^/', path, invert = TRUE)
+    path[relative_paths] = file.path(getwd(), path[relative_paths])
+
+    # In the following, we have to be careful not to remove `.` or `..` when
+    # they are part of a path component name, e.g. `.bashrc`.
+
+    path = gsub('(?<=/)\\.(/|$)', '', path, perl = TRUE)
+
+    # The following needs to be in a loop rather than a global search to
+    # correctly handle subsequent occurrences of `..`, e.g. `foo/../..`.
+    while (any(grepl('/\\.{2}(/|$)', path))) {
+        # Remove a single `..`
+        path = sub('/([^/]|\\\\/)+/\\.{2}(/|$)', '/', path)
+    }
+
+    # Collapse slashes
+    path = gsub('//+', '/', path)
+
+    # Remove trailing slashes
+    sub('(.+)/$', '\\1', path)
 }
 
 expect_paths_equal = function (actual, expected) {
