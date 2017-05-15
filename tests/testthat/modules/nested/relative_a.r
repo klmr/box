75762867f5bc27e5b6a6ca23bdf90df40713a9c3
Konrad Rudolph <konrad.rudolph@gmail.com>
2017-05-15 23:40:04 +0100

    Muffle messages in test

--- tests/testthat/modules/nested/relative_a.r
+++ tests/testthat/modules/nested/relative_a.r
@@ -1,5 +1,14 @@
 a = import('a')
-local_a = import('./a')
+
+# Muffle message
+silently = function (expr) {
+    on.exit({sink(); close(file)})
+    file = textConnection('out', 'w', local = TRUE)
+    sink(file)
+    expr
+}
+
+local_a = silently(import('./a'))
 
 a_which = function () a$which()
 
