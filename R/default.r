3c5bb736db703a4b06a73e09929b295f92439a64
Konrad Rudolph <konrad.rudolph@gmail.com>
2017-10-04 16:32:23 +0100

    Parse the module specification passed to `use`
    `parse_mod_spec` and the dependent functions parse the AST of the
    unevaluated expression being passed to the function `use` (or whatever
    it ends up being called) by the user, and which specifies the module or
    package name, alias and import attachment list.

---
+++ R/default.r
@@ -0,0 +1,35 @@
+#' Retrieve a value or a default
+#'
+#' \code{a \%||\% b} returns \code{a} unless it is empty, in which case
+#' \code{b} is returned.
+#' @param a the value to return if non-empty
+#' @param b default value
+#' @return \code{a \%||\% b} returns \code{a}, unless it is \code{NA},
+#' \code{NULL}, \code{FALSE} or \code{""}; in which case \code{b} is returned.
+#' @name default
+`%||%` = function (a, b) {
+    is_false = function (a) is.logical(a) && ! isTRUE(a)
+    is_empty = function (a) is.character(a) && ! nzchar(a)
+    if (is.null(a) || is.na(a) || is_false(a) || is_empty(a)) {
+        b
+    } else {
+        a
+    }
+}
+
+#' \code{lhs \%|\% rhs} is a vectorized version.
+#' @param lhs vector with potentially missing values, or \code{NULL}
+#' @param rhs vector with default values, same length as \code{lhs} unless that
+#'  is \code{NULL}
+#' @return \code{lhs \%|\% rhs} returns a vector of the same length as
+#' \code{rhs} with all missing values in \code{lhs} replaced by the
+#' corresponding values in \code{rhs}.
+#' @rdname default
+#' @keywords internal
+`%|%` = function (lhs, rhs) {
+    if (is.null(lhs)) {
+        rhs
+    } else {
+        mapply(`%||%`, lhs, rhs, USE.NAMES = FALSE)
+    }
+}
---
+++ R/parse_mod_spec.r
@@ -0,0 +1,148 @@
+# Module/package specification expression grammar as a PEG, approximating the R
+# parse tree:
+#
+# mod_spec    → pkg_name (“[” attach_spec “]”)? !“/” /
+#               mod
+# pkg_name    → identifier
+# mod         → mod_prefix “/” mod_name (“[” attach_spec “]”)?
+# mod_prefix  → identifier !“/” /
+#               mod_prefix “/” identifier
+# mod_name    → identifier
+# attach_spec → “...” /
+#               attach_list
+# attach_list → identifier (“,” identifier)+
+# identifier  → ‹valid R identifier›
+#
+# Note: This is missing the alias declaration, since that is not captured in the
+# AST provided by R. It is instead handled separately.
+
+#' Parse a mod spec expression passed to \code{use}
+#'
+#' @param expr the mod spec expression to parse
+#' @return \code{parse_mod_spec} returns a named list that contains information
+#' about the parsed mod specification. Currently it contains:
+#' \describe{
+#'  \item{\code{mod}}{the module information, if the spec is a module; \emph{or}}
+#'  \item{\code{pkg}}{the package information, if the spec is a package}
+#'  \item{\code{alias}}{the module/package alias}
+#'  \item{\code{attach}}{a named vector of symbols to attach, or
+#'      \code{TRUE} to attach all symbols, or \code{NULL} to attach nothing}
+#' }
+#' @keywords internal
+parse_mod_spec = function (...) {
+    expr = substitute(list(...))
+    alias = names(expr[-1])
+    mod_spec = try(parse_mod_spec_impl(expr[[2]]), silent = TRUE)
+
+    if (inherits(mod_spec, 'try-error')) {
+        # Pretty-print error call to be informative for the user.
+        cond = attr(mod_spec, 'condition')
+        cond$call = `[[<-`(expr, quote(use))
+        stop(cond)
+    }
+    c(mod_spec, alias = alias %||% (mod_spec$mod %||% mod_spec$pkg)$name)
+}
+
+parse_mod_spec_impl = function (expr) {
+    if (is.name(expr)) {
+        c(parse_pkg_name(expr), list(attach = NULL))
+    } else if (is.call(expr)) {
+        if (identical(expr[[1]], quote(`[`))) {
+            c(parse_pkg_name(expr[[2]]), parse_attach_spec(expr))
+        } else if (identical(expr[[1]], quote(`/`))) {
+            parse_mod(expr)
+        } else {
+            parse_error('Unexpected token in ', expr)
+        }
+    } else {
+        parse_error('Unexpected token in ', expr)
+    }
+}
+
+parse_pkg_name = function (expr) {
+    list(pkg = list(name = parse_identifier(expr)))
+}
+
+parse_mod = function (expr) {
+    prefix = parse_mod_prefix(expr[[2]])
+    mod = expr[[3]]
+
+    if (is.call(mod)) {
+        if (identical(mod[[1]], quote(`[`))) {
+            list(
+                mod = c(parse_mod_name(mod[[2]]), prefix),
+                parse_attach_spec(mod)
+            )
+        } else {
+            parse_error('Expected module name or attach list, got ', mod)
+        }
+    } else if (is.name(mod)) {
+        list(
+            mod = c(parse_mod_name(mod), prefix),
+            attach = NULL
+        )
+    } else {
+        parse_error('Expected module name or attach list, got ', mod)
+    }
+}
+
+parse_mod_prefix = function (expr) {
+    if (is.name(expr)) {
+        list(prefix = deparse(expr))
+    } else if (is.call(expr) && identical(expr[[1]], quote(`/`))) {
+        if (! is.name(expr[[3]])) {
+            parse_error('Expected identifier in module prefix, got ', expr[[3]])
+        } else {
+            suffix = deparse(expr[[3]])
+            list(prefix = c(parse_mod_prefix(expr[[2]])$prefix, suffix))
+        }
+    } else {
+        parse_error('Expected module prefix, got ', expr)
+    }
+}
+
+parse_mod_name = function (expr) {
+    list(name = parse_identifier(expr))
+}
+
+parse_attach_spec = function (expr) {
+    assign_missing_names = function (lst) {
+        setNames(lst, names(lst) %|% unlist(lst))
+    }
+
+    items = parse_attach_list(expr[-1][-1])
+    if (length(items) == 1 && identical(items[[1]], '...')) {
+        list(attach = TRUE)
+    } else {
+        list(attach = assign_missing_names(items))
+    }
+}
+
+parse_attach_list = function (expr) {
+    if (length(expr) == 1L && identical(expr[[1]], quote(expr = ))) {
+        parse_error('Expected at least one identifier in attach list')
+    } else {
+        unlist(lapply(expr, parse_identifier), recursive = FALSE)
+    }
+}
+
+parse_identifier = function (expr) {
+    if (length(expr) != 1L || ! is.name(expr)) {
+        parse_error('Expected identifier, got ', expr)
+    } else if (identical(expr, quote(expr = ))) {
+        parse_error('Expected identifier, got nothing')
+    } else {
+        list(deparse(expr))
+    }
+}
+
+parse_error = function (...) {
+    chr = function (x) {
+        if (is.recursive(x) || is.pairlist(x)) {
+            deparse(x)
+        } else {
+            as.character(x)
+        }
+    }
+    stop(paste0(vapply(list(...), chr, character(1))))
+}
