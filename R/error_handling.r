c95000ba7aac3cb94a109b4a64a7b255c175b703
Konrad Rudolph <konrad.rudolph@gmail.com>
2018-01-07 11:08:56 +0000

    Add general error-handling routines

---
+++ R/error_handling.r
@@ -0,0 +1,18 @@
+#' Pretty-print error call to be informative for the user.
+#'
+#' @param error an object of class \code{c("error", "condition")} to rethrow
+#' @param call the calling context to rethrow the error from, overwriting the
+#' error’s current call object
+#' @keywords internal
+rethrow = function (error, call = sys.call(sys.parent())) {
+    error$call = call
+    stop(error)
+}
+
+#' @param expr an expression to evaluate inside \code{tryCatch}
+#' @return If it does not throw an error, \code{rethrow_on_error} returns the
+#' value of evaluating \code{expr}.
+#' @rdname rethrow
+rethrow_on_error = function (expr, call = sys.call(sys.parent())) {
+    tryCatch(expr, error = function (error) rethrow(error, call))
+}
