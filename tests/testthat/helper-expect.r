fe6f56c89bc66763d72c320188e6f75d46ad0484
Konrad Rudolph <konrad.rudolph@gmail.com>
2018-08-10 20:03:01 +0100

    Add another test helper, and test for the helpers

--- tests/testthat/helper-expect.r
+++ tests/testthat/helper-expect.r
@@ -3,14 +3,32 @@
 
 options(mod.path = 'modules')
 
-#' Opposite of \code{is_identical_to}
 expect_not_identical = function (object, expected, info = NULL, label = NULL, expected.label = NULL) {
     lab_act = testthat::quasi_label(rlang::enquo(object), label)
     lab_exp = testthat::quasi_label(rlang::enquo(expected), expected.label)
     ident = identical(object, expected)
 
     msg = if (ident) 'Objects identical' else ''
-    testthat::expect_false(ident, info = info,
-                           label = sprintf('%s identical to %s.\n%s',
-                                           lab_act, lab_exp, msg))
+    testthat::expect_false(
+        ident, info = info,
+        label = sprintf('%s identical to %s.\n%s', lab_act, lab_exp, msg)
+    )
+}
+
+expect_in = function (name, list) {
+    act = testthat::quasi_label(rlang::enquo(list))
+    testthat::expect(
+        name %in% list,
+        sprintf('%s is not in %s.', deparse(name), act$lab)
+    )
+    invisible(act$val)
+}
+
+expect_not_in = function (name, list) {
+    act = testthat::quasi_label(rlang::enquo(list))
+    testthat::expect(
+        ! name %in% list,
+        sprintf('%s is in %s.', deparse(name), act$lab)
+    )
+    invisible(act$val)
 }
