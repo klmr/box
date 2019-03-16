9274decd405e5bd8ead3fa75a3ba2e19c9e4f7e2
Konrad Rudolph <konrad.rudolph@gmail.com>
2019-03-16 17:36:49 +0000

    Remove test of obsolete/removed behavior

--- tests/testthat/test-nested.r
+++ tests/testthat/test-nested.r
@@ -9,29 +9,16 @@ test_that('submodules can be loaded one by one', {
     on.exit(teardown())
 
     result = capture.output(mod::use(mod/nested/a/b))
-    expect_that(result, equals(c('a/__init__.r', 'a/b/__init__.r')))
+    expect_equal(result, 'a/b/__init__.r')
 
     result = capture.output(mod::use(mod/nested/a/b/c))
-    expect_that(result, equals('a/b/c/__init__.r'))
+    expect_equal(result, 'a/b/c/__init__.r')
 
     result = capture.output(mod::use(mod/nested/a/b/d))
-    expect_that(result, equals('a/b/d/__init__.r'))
-
-    result = capture.output(mod::use(mod/nested/a/b/c/e))
-    expect_that(result, equals('a/b/c/e.r'))
+    expect_equal(result, 'a/b/d/__init__.r')
 })
 
 test_that('module can export nested submodules', {
-    b = mod::use(mod/b)
+    mod::use(mod/b)
     expect_equal(b$answer, 42L)
-})
-
-test_that('submodules load all relevant init files', {
-    on.exit(teardown())
-
-    result = capture.output(mod::use(mod/nested/a/b/d))
-    expect_that(result, equals(c('a/__init__.r', 'a/b/__init__.r', 'a/b/d/__init__.r')))
-
-    result = capture.output(mod::use(mod/nested/a/b/c/e))
-    expect_that(result, equals(c('a/b/c/__init__.r', 'a/b/c/e.r')))
 })
