klmr/box

commit a57172d0020e18ec038a0f50d0a1da72eff57d57

Make exports work inside cyclic module dependency
Exporting a use spec now collects the reexported *names* rather than the
reexported *definitions* of the use spec. This allows determining the
reexported names even during a cyclic import resolution, where the
definitions are not yet available.

R/use.r
@@ -179,16 +179,8 @@ load_mod.pkg_info = function (info) {
 }
 
 mod_exports = function (info, spec, ns) {
-    exports = if (inherits(info, 'pkg_info'))
-        getNamespaceExports(ns) else
-        namespace_info(ns, 'exports')
-
-    # BUG: This fails to distinguish between deferred exporting and a genuinely
-    # empty export list, which can happen.
-    if (is.null(exports)) {
-        # FIXME: defer
-        return()
-    }
+    exports = mod_export_names(info, ns)
+    if (is.null(exports)) return()
 
     env = make_export_env(info, spec, ns)
     list2env(mget(exports, ns, inherits = TRUE), envir = env)
@@ -196,6 +188,22 @@ mod_exports = function (info, spec, ns) {
     # FIXME: Lock namespace and/or ~-bindings?
     # lockEnvironment(env, bindings = TRUE)
     env
+}
+
+#' @return \code{mode_export_names} returns the same as
+#' \code{names(mod_exports(info, spec, ns))} and does not create an export environment.
+#' @name mod_exports
+#' @keywords internal
+mod_export_names = function (info, ns) {
+    UseMethod('mod_export_names')
+}
+
+mod_export_names.mod_info = function (info, ns) {
+    namespace_info(ns, 'exports')
+}
+
+mod_export_names.pkg_info = function (info, ns) {
+    getNamespaceExports(ns)
 }
 
 attach_to_caller = function (spec, mod_exports, caller) {

R/export.r
@@ -70,9 +70,9 @@ parse_export_specs = function (info, mod_ns) {
             return(spec$alias)
         }
 
-        exports = mod_exports(info, spec, import_ns)
+        export_names = mod_export_names(info, import_ns)
         real_alias = if (is.null(spec$attach) || spec$explicit) spec$alias
-        c(names(attach_list(spec, exports)), real_alias)
+        c(names(attach_list(spec, export_names)), real_alias)
     }
 
     find_matching_import = function (imports, reexport) {

commit 21646dd5619b8e8417c97561657c244946db704e

Fix regression introduced in a57172d0

R/use.r
@@ -210,7 +210,7 @@ mod_export_names.pkg_info = function (info, ns) {
 }
 
 attach_to_caller = function (spec, mod_exports, caller) {
-    attach_list = attach_list(spec, mod_exports)
+    attach_list = attach_list(spec, ls(mod_exports))
     if (is.null(attach_list)) return()
 
     import_env = find_import_env(caller, spec)
@@ -224,7 +224,7 @@ attach_list = function (spec, exports) {
     is_wildcard = is.na(spec$attach)
     name_spec = spec$attach[! is_wildcard]
 
-    if (! all(is_wildcard) && any((missing = ! name_spec %in% ls(exports)))) {
+    if (! all(is_wildcard) && any((missing = ! name_spec %in% exports))) {
         stop(sprintf(
             'Name%s %s not exported by %s',
             if (length(name_spec[missing]) > 1L) 's' else '',
@@ -234,17 +234,16 @@ attach_list = function (spec, exports) {
     }
 
     if (any(is_wildcard)) {
-        attach_list = ls(exports)
-        if (length(attach_list) == 0L) return()
+        if (length(exports) == 0L) return()
 
-        aliases = attach_list
+        aliases = exports
         if (length(spec$attach) > 1L) {
             # Substitute aliased names in export list
-            to_replace = match(name_spec, attach_list)
+            to_replace = match(name_spec, exports)
             aliases[to_replace] = names(name_spec)
         }
 
-        setNames(attach_list, aliases)
+        setNames(exports, aliases)
     } else {
         name_spec
     }