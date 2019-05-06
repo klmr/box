fa0af408f1a5d8de1925c2c4795f801b1af53a0b
Konrad Rudolph <konrad.rudolph@gmail.com>
2019-05-06 12:51:34 +0100

    Remove duplicate code

--- tests/testthat/test-nested.r
+++ tests/testthat/test-nested.r
@@ -1,12 +1,7 @@
 context('submodules')
 
-teardown = function () {
-    # Unload all modules
-    rm(list = ls(envir = mod:::loaded_mods), envir = mod:::loaded_mods)
-}
-
 test_that('submodules can be loaded one by one', {
-    on.exit(teardown())
+    on.exit(clear_mods())
 
     result = capture.output(mod::use(mod/nested/a/b))
     expect_equal(result, 'a/b/__init__.r')
