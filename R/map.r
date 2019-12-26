fd61af7f388f970e5f887cdaa9cf9ce0d9eb8d23
Konrad Rudolph <konrad.rudolph@gmail.com>
2019-12-26 21:59:02 +0000

    Add vector mapping functions

--- R/map.r
+++ R/map.r
@@ -34,3 +34,17 @@ flatmap = function (.f, ..., .default) {
 flatmap_chr = function (.f, ...) {
     flatmap(.f, ..., .default = character(0L))
 }
+
+vmap = function (.f, .x, ..., .default) {
+    # FIXME: Find a more efficient implementation.
+    fun_value = eval(call(class(.default), 1L))
+    vapply(X = .x, FUN = .f, FUN.VALUE = fun_value, ..., USE.NAMES = FALSE)
+}
+
+map_int = function (.f, ...) {
+    vmap(.f = .f, ..., .default = integer(0L))
+}
+
+map_lgl = function (.f, ...) {
+    vmap(.f = .f, ..., .default = logical(0L))
+}
