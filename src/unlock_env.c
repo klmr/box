d0a44acfb81ffefc47cae50a6a02ade71a897a4d
Konrad Rudolph <konrad.rudolph@gmail.com>
2021-03-12 00:02:43 +0000

    Remove dead code

--- src/unlock_env.c
+++ src/unlock_env.c
@@ -2,8 +2,7 @@
 
 // Based on <https://gist.github.com/wch/3280369>
 
-#define FRAME_LOCK_MASK (1<<14)
-#define FRAME_IS_LOCKED(e) (ENVFLAGS(e) & FRAME_LOCK_MASK)
+#define FRAME_LOCK_MASK (1 << 14)
 #define UNLOCK_FRAME(e) SET_ENVFLAGS(e, ENVFLAGS(e) & (~ FRAME_LOCK_MASK))
 
 SEXP c_unlock_env(SEXP env) {
