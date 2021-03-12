8cf7903ef1d37180ba7564222c57e1e1c4688567
Konrad Rudolph <konrad.rudolph@gmail.com>
2021-03-12 00:04:18 +0000

    Clean up `lookup.c` implementation
    * Fix static object GC preservation
    * Move utility functions below exported function
    * Remove debug logging
    * Extract helper code from main code
    * Add comments

--- src/lookup.c
+++ src/lookup.c
@@ -1,28 +1,75 @@
 #include "Rinternals.h"
-
-// [BEGIN:includes]
 #include "R_ext/Parse.h"
-// [END:includes]
 
-// [BEGIN:otherdefs]
-/* #define LOG(format, ...) \ */
-/*     fprintf(stderr, "[%s] " format, __FUNCTION__, __VA_ARGS__) */
+static SEXP parent_frame(void);
+static SEXP sys_call(SEXP parent);
 
-#define LOG(format, ...) do {} while (0)
+/**
+ * Extract a named value from an environment (called as {@code e1$e2}).
+ * @param e1 a module environment or module namespace
+ * @param e2 a length-1 character string vector that corresponds to the name.
+ *  This is a character string rather than an unevaluated name, since this is
+ *  how R dispatches the generic {@code `$`} to a method.
+ * @return the value corresponding to the name in the environment, if it exists.
+ * Throws an error if {@code e1} is not an environment, or if {@code e2} does
+ * not exist.
+ */
+SEXP c_strict_extract(SEXP e1, SEXP e2) {
+    PROTECT(e1);
+    if (! isEnvironment(e1)) {
+        UNPROTECT(1);
+        error("first argument was not a module environment");
+        return R_NilValue;
+    }
 
-static SEXP current_frame_fun = NULL;
+    PROTECT(e2);
+    SEXP name = PROTECT(installTrChar(STRING_ELT(e2, 0)));
+    SEXP ret = findVarInFrame(e1, name);
 
-static void init_frame(void);
+    if (ret == R_UnboundValue) {
+        SEXP parent = PROTECT(parent_frame());
+        SEXP call = PROTECT(sys_call(parent));
+        SEXP fst_arg = PROTECT(CADR(call));
 
-static SEXP current_frame(void) {
-    LOG("current_frame_fun = %p\n", (void*) current_frame_fun);
-    if (! current_frame_fun) init_frame();
-    LOG("current_frame_fun = %p\n", (void*) current_frame_fun);
-    return Rf_eval(current_frame_fun, R_EmptyEnv);
+        errorcall(
+            call, "name '%s' not found in '%s'",
+            translateChar(STRING_ELT(e2, 0)),
+            translateChar(PRINTNAME(fst_arg))
+        );
+        UNPROTECT(6);
+        return R_NilValue;
+    }
+
+    UNPROTECT(3);
+    return ret;
 }
 
+// Cached version of an R function that calls `sys.frame(-1L)`.
+static SEXP parent_frame_func = NULL;
+
+static void init_parent_frame_func(void);
+
+// Return the calling R frame.
+static SEXP parent_frame(void) {
+    if (! parent_frame_func) init_parent_frame_func();
+    return Rf_eval(parent_frame_func, R_EmptyEnv);
+}
+
+// Return the call that describes the R function which invoked the parent
+// function that calls this C function, identified by `parent`.
+static SEXP sys_call(SEXP parent) {
+    ParseStatus status;
+    SEXP code = PROTECT(Rf_mkString("sys.call(-1L)"));
+    SEXP expr = PROTECT(R_ParseVector(code, -1, &status, R_NilValue));
+    SEXP func = VECTOR_ELT(PROTECT(Rf_eval(expr, R_BaseEnv)), 0);
+    SEXP call = Rf_eval(func, parent);
+
+    UNPROTECT(3);
+    return call;
+}
+
+// Create a new R closure from the given formals and body.
 static SEXP new_function(SEXP formals, SEXP body) {
-    LOG("formals = %p, body = %p\n", (void*) formals, (void*) body);
     SEXP def_args = PROTECT(Rf_cons(formals, PROTECT(Rf_cons(body, R_NilValue))));
     SEXP def_expr = PROTECT(Rf_lcons(Rf_install("function"), def_args));
     SEXP fun = Rf_eval(def_expr, R_BaseEnv);
@@ -31,55 +78,15 @@ static SEXP new_function(SEXP formals, SEXP body) {
     return fun;
 }
 
-static void init_frame(void) {
+static void init_parent_frame_func(void) {
     ParseStatus status;
     SEXP code = PROTECT(Rf_mkString("as.call(list(sys.frame, -1L))"));
-    LOG("code = %p\n", code);
     SEXP expr = PROTECT(VECTOR_ELT(PROTECT(R_ParseVector(code, -1, &status, R_NilValue)), 0));
-    LOG("expr = %p\n", expr);
     SEXP body = PROTECT(Rf_eval(expr, R_BaseEnv));
-    LOG("body = %p\n", body);
     SEXP func = PROTECT(new_function(R_NilValue, body));
-    LOG("func = %p\n", func);
-    current_frame_fun = PROTECT(Rf_lcons(func, R_NilValue));
+    parent_frame_func = Rf_lcons(func, R_NilValue);
+    R_PreserveObject(parent_frame_func);
+    MARK_NOT_MUTABLE(parent_frame_func);
 
     UNPROTECT(5);
-}
-// [END:otherdefs]
-
-SEXP c_strict_extract(SEXP e1, SEXP e2) {
-// [BEGIN:body]
-    LOG("e1 = %p, e2 = %p\n", e1, e2);
-    PROTECT(e1);
-    if (! isEnvironment(e1)) {
-        UNPROTECT(1);
-        error("first argument was not a module environment");
-        return R_NilValue;
-    }
-    PROTECT(e2);
-    LOG("length(e2) = %d\n", LENGTH(e2));
-    SEXP name = PROTECT(installTrChar(STRING_ELT(e2, 0)));
-    SEXP ret = findVarInFrame(e1, name);
-    LOG("ret = %p\n", ret);
-    if (ret == R_UnboundValue) {
-        SEXP parent = PROTECT(current_frame());
-        LOG("parent = %p\n", parent);
-        ParseStatus status;
-        SEXP call_code = PROTECT(Rf_mkString("sys.call(-1L)"));
-        SEXP call_expr = PROTECT(R_ParseVector(call_code, -1, &status, R_NilValue));
-        SEXP call = VECTOR_ELT(PROTECT(Rf_eval(call_expr, parent)), 0);
-        call = PROTECT(Rf_eval(call, parent));
-        SEXP fst_arg = PROTECT(CADR(call));
-
-        errorcall(
-            call, "name '%s' not found in '%s'",
-            translateChar(STRING_ELT(e2, 0)),
-            translateChar(PRINTNAME(fst_arg))
-        );
-        UNPROTECT(9);
-        return R_NilValue;
-    }
-    UNPROTECT(3);
-    return ret;
-// [END:body]
 }
