a6669448ace7054a73bee16606e3ab6381f72391
Konrad Rudolph <konrad.rudolph@roche.com>
2024-02-05 15:52:23 +0100

    Fix interactive testing with user-provided .First
    Setting `options('prompt')` needs to happen after the user Rprofile had
    a chance to touch it, which may happen after `.Rprofile` finished
    loading, in `.First()`.

--- tests/testthat/support/rprofile.r
+++ tests/testthat/support/rprofile.r
@@ -22,8 +22,11 @@ local({
     if (file.exists(user_profile)) {
         sys.source(user_profile, envir = .GlobalEnv)
     }
+
+    old_first = get0('.First', envir = .GlobalEnv, ifnotfound = function () {})
+    .GlobalEnv$.First = function () {
+        old_first()
+        # This is required by `interactive_r` to verify that invocation succeeded.
+        options(prompt = '> ', continue = '+ ')
+    }
 })
-
-# This is required by `interactive_r` to verify that invocation succeeded.
-
-options(prompt = '> ')
